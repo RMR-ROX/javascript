da550cad29c81c264506781cc5759752c6750ff0
RMR-ROX <143639456+RMR-ROX@users.noreply.github.com>
2024-07-17 08:22:38 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict";// treats all JS code as newer version
+
+// alert( 3+3)// works only on browser not node js
+
+// console.log(3+3); console.log("Riddhi")/. do not do these code readeability is harmed
+
+//node 01_basics/02_datatypes.js
+
+let age=18
+let name="riddhi"
+let bool=true
+
+
+
+console.table([age,name,bool])
+let state=null// null stand alone value which implies empty
+
+//number = 2 to the power 53
+//bigint
+//string=> ""
+//boolean
+//null =>standalone value implies empty
+//undefined
+// symbol => unique
+
+
+
+//object
+
+console.log(typeof(null))// output- Object
+
+
+
+
+
+
