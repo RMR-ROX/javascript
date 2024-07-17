dc3377f858e9f3ee0375e5d6e312913c77f6a267
RMR-ROX <143639456+RMR-ROX@users.noreply.github.com>
2024-07-17 08:41:14 +0000

    conversion of data types

---
+++ 01_basics/01_conversionOperations.js
@@ -0,0 +1,31 @@
+// let score=false
+
+// console.log(typeof(score))
+
+// let valInNumber = Number(score);// converts string to number
+
+// console.log(typeof valInNumber);
+// console.log(valInNumber);
+
+// "33" => converts to number 33
+// "33abc" =>NaN
+//....similarly for the rest data types
+
+
+// let isLoggedIn="riddhi"
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 =>false
+//"" => false
+//"riddhi" => true
+
+
+let someNumber=33
+
+let stringnum=String(someNumber);
+console.log(stringnum);
+console.log(typeof stringnum)
+
+
+
